85075e20cf84d8943e1013b7c705d7a3485da993
Kash Patel <ksp.sdd.ptl@gmail.com>
2021-07-31 03:08:57 -0400

    Program reads requests from input file into max array

--- Question1.c
+++ Question1.c
@@ -8,8 +8,8 @@
 #include <time.h>
 
 
-int processes = 0;
-int resourcesTypes = 0;
+int numProcesses = 0;
+int numResourcesTypes = 4;
 
 int *available;
 int **allocated;
@@ -20,12 +20,39 @@ int *safeSeq;
 char *inputString;
 
 
+int readResources(char *argv[]);
 int readRequests();
 
 
 int main (int argc, char *argv[]) {
 
+  readResources(argv);
   readRequests();
+
+  printf("Number of Customers: %d\n", numProcesses);
+  printf("Currently Available resources: ");
+  for (size_t i = 0; i < argc - 1; i++) {
+    printf("%d ", available[i]);
+  } printf("\n");
+  printf("Maximum resources from file:\n");
+  for (size_t i = 0; i < numProcesses; i++) {
+    for (size_t ii = 0; ii < numResourcesTypes; ii++) {
+      printf("%d ", max[i][ii]);
+    } printf("\n");
+  }
+  
+
+  return 0;
+}
+
+
+int readResources (char *argv[]) {
+
+  available = (int *) malloc(4 * sizeof(int));
+
+  for (size_t i = 1; i < 5; i++) {
+    available[i - 1] = atoi(argv[i]);
+  }
 
   return 0;
 }
@@ -41,14 +68,34 @@ int readRequests () {
     return -1;
   }
 
+  max = (int **) malloc(5 * sizeof(int *)); // Initialise to 5 processes for now
   char line[100];
 
   while (fgets(line, sizeof(line), in)) {
-    printf("%s", line);
+
+    max[numProcesses] = (int *) malloc(4 * sizeof(int));
+    
+    char *tempLine = (char *) malloc(sizeof(line) * sizeof(char));
+    strcpy(tempLine, line);
+    char *token = strtok(tempLine, ",");
+
+    int i = 0;
+
+    while (token != NULL) {
+
+      max[numProcesses][i] = atoi(token);
+
+      token = strtok(NULL, ",");
+
+      i++;
+
+    }
+
+    numProcesses++;
   }
 
   fclose(in);
 
   return 0;
-  
+
 }
